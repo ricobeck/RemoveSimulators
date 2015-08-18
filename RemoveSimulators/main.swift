e19ddf1ec58147b5f09aba32b2d9e5e03c1b41d0
rico becker <rico.becker@kf-interactive.com>
2015-08-18 08:32:55 +0200

    Initial Commit

---
+++ RemoveSimulators/main.swift
@@ -0,0 +1,66 @@
+//
+//  main.swift
+//  RemoveSimulators
+//
+//  Created by rick on 18/08/15.
+//  Copyright © 2015 KF Interactive Gmbh. All rights reserved.
+//
+
+import Foundation
+
+
+func getDeviceListOutput() -> String? {
+    let task = NSTask()
+    task.launchPath = "/usr/bin/xcrun"
+    task.arguments = ["simctl", "list"]
+    
+    let pipe = NSPipe()
+    task.standardOutput = pipe
+    task.launch()
+    task.waitUntilExit()
+    
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    if let output = NSString(data: data, encoding: NSUTF8StringEncoding) {
+        return output as String
+    } else {
+        return String?()
+    }
+}
+
+func deleteDeviceWithUuid(uuid: String) {
+    print("deleting: \(uuid)")
+    let task = NSTask()
+    task.launchPath = "/usr/bin/xcrun"
+    task.arguments = ["simctl", "delete", uuid]
+    
+    let pipe = NSPipe()
+    task.standardOutput = pipe
+    task.launch()
+    task.waitUntilExit()
+    
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    if let output = NSString(data: data, encoding: NSUTF8StringEncoding) {
+        print("-> \(output)")
+    }
+}
+
+func matchesForRegexInText(regex: String, text: String) -> [String] {
+    let regex = try! NSRegularExpression(pattern: regex, options: NSRegularExpressionOptions.CaseInsensitive)
+    let nsString = text as NSString
+    let results = regex.matchesInString(text, options: [], range: NSMakeRange(0, nsString.length))
+    return results.map { nsString.substringWithRange($0.rangeAtIndex(1))}
+}
+
+func deviceUuidsFromListOutput(output: String) -> [String] {
+    let result = matchesForRegexInText("\\(([0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12})\\)\\s\\(Shutdown\\)", text: output)
+    return result
+}
+
+if let output = getDeviceListOutput() {
+    let uuids = deviceUuidsFromListOutput(output)
+    
+    for uuid in uuids {
+        deleteDeviceWithUuid(uuid)
+    }
+}
+
